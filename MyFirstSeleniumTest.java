9fc9b251d6fb0458b4229e6e68a297a44e11757a
Sunil Bansal <meetsunilbansal@gmail.com>
2021-08-08 13:52:28 +0530

    Modified the file to try Git SCM Push

--- MyFirstSeleniumTest.java
+++ MyFirstSeleniumTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MyFirstSeleniumTest {
-
+	// Added a comment
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "test\\resources\\chromedriver.exe");
 		WebDriver driver  = new ChromeDriver();
